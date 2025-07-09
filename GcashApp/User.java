2feef8541812a757a93b02614575862c33f7fea3
milucy09 <39399509+milucy09@users.noreply.github.com>
2025-07-09 16:48:55 +0000

    Update GcashApp

---
+++ GcashApp/User.java
@@ -0,0 +1,10 @@
+package GcashApp;
+
+import java.util.ArrayList;
+
+public class User {
+    String mobileNumber;
+    String pin;
+    double balance;
+    ArrayList<String> TransactionHistory;    
+}
