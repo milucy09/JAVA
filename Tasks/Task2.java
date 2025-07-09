0c6299fa2dd829a72469390b46710a4d1d9112da
milucy09 <39399509+milucy09@users.noreply.github.com>
2025-07-09 15:20:58 +0000

    Update Repo Add Task2

---
+++ Tasks/Task2.java
@@ -0,0 +1,14 @@
+package Tasks;
+
+public class Task2 {
+    public static void main(String[] args) {
+        char h = 'H';
+        byte b = 3;
+        short s = 1;
+        int i = 0;
+        float f = 2.0f;
+        boolean bool = true;
+
+        System.out.println(""+ h + b + s + s + i + " world " + f + " " + bool);
+    }
+}
