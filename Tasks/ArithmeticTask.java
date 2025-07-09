662041cb2b44ef0cfe80b879851a9cdfa95bff97
milucy09 <39399509+milucy09@users.noreply.github.com>
2025-07-09 14:51:55 +0000

    Add Arithmetic Tasks Sheet 1-1-2

---
+++ Tasks/ArithmeticTask.java
@@ -0,0 +1,15 @@
+package Tasks;
+
+public class ArithmeticTask {
+    public static void main(String[] args) {
+        int x = 10;
+        
+        x += 5;
+        x -= 3;
+        x *= 2;
+        x /= 4;
+        x %= 3;
+
+        System.out.println("Final value of x is: " + x);
+    }
+}
